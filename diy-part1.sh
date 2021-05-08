498a6e081be51d531d6598472813aae70853d8b3
ギリ猫 <king@ineko.cc>
2021-05-09 00:53:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# coustom things
+git clone https://github.com/CHN-beta/rkp-ipid.git package/xmurp-ua
