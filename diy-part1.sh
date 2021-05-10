62206814060f1925ddf738d5fb4019c903ad3b4a
GiriNeko <king@ineko.cc>
2021-05-10 13:35:54 +0800

    fix config

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add some packages
+git clone https://github.com/GiriNeko/rkp-ipid package/network/utils/rkp-ipid
