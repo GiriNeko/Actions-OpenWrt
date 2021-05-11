7a6f4826798a7e11c3ee6af92472da72b0945504
ギリ猫 <king@ineko.cc>
2021-05-11 15:39:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# R4A Flash Fix
+rm -rf openwrt/target/linux/ramips/image/mt7621.mk
+wget -P openwrt/target/linux/ramips/image/mt7621.mk https://raw.githubusercontent.com/GiriNeko/Actions-OpenWrt/XiaoMi_R4A_Gigabyte/mt7621.mk
+rm -rf openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+wget -P openwrt/target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts https://raw.githubusercontent.com/GiriNeko/Actions-OpenWrt/XiaoMi_R4A_Gigabyte/mt7621_xiaomi_mir3g-v2.dts
