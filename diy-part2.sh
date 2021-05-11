fa03b12156c9eba201e5a29dc574c0023c231665
ギリ猫 <king@ineko.cc>
2021-05-11 16:06:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,5 @@
 
 # R4A Flash Fix
 wget https://raw.githubusercontent.com/GiriNeko/Actions-OpenWrt/XiaoMi_R4A_Gigabyte/mt7621.mk -O target/linux/ramips/image/mt7621.mk
-wget https://raw.githubusercontent.com/GiriNeko/Actions-OpenWrt/XiaoMi_R4A_Gigabyte/mt7621_xiaomi_mir3g-v2.dts -O target/linux/ramips/image/mt7621.mk
+wget https://raw.githubusercontent.com/GiriNeko/Actions-OpenWrt/XiaoMi_R4A_Gigabyte/mt7621_xiaomi_mir3g-v2.dts -O target/linux/ramips/dts/mt7621_xiaomi_mir3g-v2.dts
+
